6dc539c9524689e0615e87a3839a8427efae8f58
Joey <joseph.r.young2002@gmail.com>
2021-07-28 16:01:12 -0500

    got yes or no set in JS

--- Javascript/app3.js
+++ Javascript/app3.js
@@ -7,20 +7,36 @@ var number = 7;
 
 
 
+var yes = prompt("pick a number between 1 and 15");
 
-while(chances > 0){
-  var guess = prompt("pick a number between 1 and 15");
-
+while(chances > 1){
+  var guess = parseInt(yes, 10);
   if (guess === number) {
-    alert("You are correct! It took you " + tries + " guesses!")
-   } else {
-    if (guess < number) {
-    alert("Too low! try again!")
-     tries += 1;
-    }
-  if (guess > number) {
-     alert("Too high! try again!")
-     tries += 1;
-   }
+    alert("You are correct!");
+    break;
+  } else {
+    chances--;
+    yes = prompt("Try again!");
+    
   }
-  }
+   
+   
+   
+   
+   
+   
+  // rewrite to handle if something is too high or too low.  
+  // else{
+  // if (guess < number); {
+  //   alert("Too low! try again!");
+  //    chances -= 1;
+  //   }
+  // if (guess > number); {
+  //    alert("Too high! try again!");
+  //    chances -= 1;
+  //  }
+  // }
+  
+
+
+}
